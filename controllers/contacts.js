598d31b7a61671b365f4ae6ff141dcbc7ca6b0c9
Ebenézer Dorneles <ebenezerdorneles@gmail.com>
2021-11-16 13:54:00 -0300

    Ajuste para o chat e refatoração

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,56 +1,53 @@
-module.exports = function(app){
-	const ContactController = {
+module.exports = function (app) {
+  const ContactController = {
+    index: function (req, res) {
+      const user = req.session.user;
+      const contacts = req.session.user.contacts;
+      const params = { user: user, contacts: contacts };
 
-		index: function(req, res){
-			const user = req.session.user;
-			const contacts = req.session.user.contacts;
-			const params = {user:user, 
-											contacts: contacts};
+      res.render("contacts/index", params);
+    },
 
-			res.render('contacts/index', params);
-	
-			},
-		
-		create: function(req, res){
-			const contact = req.body.contact;
-			const user = req.session.user;
+    create: function (req, res) {
+      const contact = req.body.contact;
+      const user = req.session.user;
 
-			user.contacts.push(contact)
-			res.redirect('/contacts');
-			},
+      user.contacts.push(contact);
+      res.redirect("/contacts");
+    },
 
-		show:function(req, res){
-			const id = req.params.id;
-			const contact = req.session.user.contacts[id];
-			const params = {contact: contact, id: id};
+    show: function (req, res) {
+      const id = req.params.id;
+      const contact = req.session.user.contacts[id];
+      const params = { contact: contact, id: id };
 
-			res.render('contacts/show', params);
-			},
+      res.render("contacts/show", params);
+    },
 
-		edit: function(req, res){
-			const id = req.params.id;
-			const user = req.session.user
-			const contact = user.contacts[id];
-			const params = {user: user, contact: contact, id: id};
+    edit: function (req, res) {
+      const id = req.params.id;
+      const user = req.session.user;
+      const contact = user.contacts[id];
+      const params = { user: user, contact: contact, id: id };
 
-			res.render('contacts/edit', params);
-			},
+      res.render("contacts/edit", params);
+    },
 
-		update: function(req, res){
-			const contact = req.body.contact;
-			const user = req.session.user;
+    update: function (req, res) {
+      const contact = req.body.contact;
+      const user = req.session.user;
 
-			user.contacts[req.params.id] = contact;
-			res.redirect('/contacts');
-			},
+      user.contacts[req.params.id] = contact;
+      res.redirect("/contacts");
+    },
 
-		destroy: function(req, res){
-			const user = req.session.user;
-			const id = req.params.id;
+    destroy: function (req, res) {
+      const user = req.session.user;
+      const id = req.params.id;
 
-			user.contacts.splice(id, 1);
-			res.redirect('/contacts');
-			}
-		}
-	return ContactController;
-}
+      user.contacts.splice(id, 1);
+      res.redirect("/contacts");
+    },
+  };
+  return ContactController;
+};
--- controllers/home.js
+++ controllers/home.js
@@ -1,28 +1,27 @@
-module.exports = function(app){
-	const HomeController = {
-		index: function(req, res){
-			res.render('home/index');
-		},
-		
-		login: function(req, res){
-			const user = req.body.user;
-			const email = req.body.user.email;
-			const name = req.body.user.name;
+module.exports = function (app) {
+  const HomeController = {
+    index: function (req, res) {
+      res.render("home/index");
+    },
 
-			if(email && name){
-				user.contacts = [];
-				req.session.user = user;
-				res.redirect('/contacts');
-			}else{
-				res.redirect('/');
-			}
-		},
+    login: function (req, res) {
+      const user = req.body.user;
+      const email = req.body.user.email;
+      const name = req.body.user.name;
 
-		logout: function(req, res){
-			req.session.destroy();
-			res.redirect('/');
-		}
-	
-	};
-	return HomeController;
+      if (email && name) {
+        user.contacts = [];
+        req.session.user = user;
+        res.redirect("/contacts");
+      } else {
+        res.redirect("/");
+      }
+    },
+
+    logout: function (req, res) {
+      req.session.destroy();
+      res.redirect("/");
+    },
+  };
+  return HomeController;
 };
