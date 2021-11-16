56ff84fdbc9381d00e42cdeaebcc95a0bc70482f
Ebenézer Dorneles Da Silva <ebenezerdorneles@gmail.com>
2021-11-16 10:11:07 -0300

    ajuste as configurações

--- app.js
+++ app.js
@@ -1,51 +1,67 @@
-const express = require('express');
-const consign = require('consign');
-const http = require('http');
-const socketIO = require('socket.io');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const expressSession = require('express-session');
-const methodOverride = require('method-override');
-const error = require('./middleware/error');
+const express = require("express");
+const path = require("path");
+const consign = require("consign");
+const http = require("http");
+const socketIO = require("socket.io");
+const cookieParser = require("cookie-parser");
+const cookie = require("cookie");
+const bodyParser = require("body-parser");
+const expressSession = require("express-session");
+const methodOverride = require("method-override");
+const error = require("./middleware/error");
+const config = require("./config");
 
 const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
-
+const store = new expressSession.MemoryStore();
 
 //config
-	//template engine
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'ejs');
-	app.use(cookieParser('server_3'));
-	app.use(expressSession({resave:true, saveUninitialized: true, secret: 'server_3'}));
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({extended:true}));
-	app.use(methodOverride('_method'))
-	app.use(express.static(__dirname + '/public'));
+//template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(cookieParser("server_3"));
+app.use(
+  expressSession({
+    resave: true,
+    saveUninitialized: true,
+    name: config.sessionkey,
+    secret: config.sessionSecret,
+  })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
+io.use((socket, next) => {
+  const cookieData = socket.request.headers.cookie;
+  const cookieObj = cookie.parse(cookieData);
+  const sessionHash = cookieObj[config.sessionkey] || "";
+  const sessionID = sessionHash.split(".")[0].slice(2);
 
-	//routes
-	consign({})
-		.include('models')
-		.then('controllers')
-		.then('routes')
-		.into(app);
-	
+  store.all((err, sessions) => {
+    const currentSession = sessions[sessionID];
+    if (err || !currentSession) {
+      return next(new Error("Acesso negado!"));
+    }
+    socket.handshake.session = currentSession;
+    return next();
+  });
+});
 
-	app.use(error.notFound);
-	app.use(error.serverError);
-	
-	io.sockets.on('connection', (client) => {
-		client.on('send-server', (data) => {
-			var msg = "<b>" + data.name + ":</b> " + data.msg + "<br>";
-			client.emit('send-client', msg);
-			client.broadcast.emit('send-client', msg);
-		});
-	});
+//routes
+consign({})
+  .include("models")
+  .then("controllers")
+  .then("routes")
+  .then("sockets")
+  .into(app, io);
 
-	//port
-	server.listen(3000, () => {
-		console.log(`Estamos rodando na porta 3000!`);
-	});
+app.use(error.notFound);
+app.use(error.serverError);
 
+//port
+server.listen(3000, () => {
+  console.log(`Estamos rodando na porta 3000!`);
+});
